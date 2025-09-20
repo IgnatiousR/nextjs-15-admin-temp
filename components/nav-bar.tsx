63c660631aa8a1879b9ff6d8b452f206dfeb5499
Ignatious Rozario <ignatiousr80@gmail.com>
2025-09-20 21:34:47 +0600

    Create nav-bar.tsx

---
+++ components/nav-bar.tsx
@@ -0,0 +1,45 @@
+import { ChevronRightIcon, MoonIcon } from "lucide-react";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+
+const NavBar = () => {
+  return (
+    <nav className="p-4 flex items-center justify-between">
+      {/* Collapsebutton */}
+      <Button>
+        <ChevronRightIcon />
+      </Button>
+      <div className="flex items-center gap-4">
+        <Link href="/">Home</Link>
+        <MoonIcon />
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
